d4a31e4147a243f848be14fcbb2a1a84b37f26e9
Leonardo Santos <leobrandonli21@gmail.com>
2023-12-20 19:33:25 -0300

    feat: collected animation

--- scenes/collectable.gd
+++ scenes/collectable.gd
@@ -1,8 +1,13 @@
 extends Area2D
-
+@onready var sprite_2d = $AnimatedSprite2D
 @onready var game_manager = %GameManager
+var collected = false
 
 func _on_body_entered(body):
-	if body.name == "CharacterBody2D":
-		queue_free()
+	if body.name == "CharacterBody2D" and not collected:
+		sprite_2d.animation = "collected"
 		game_manager.add_point()
+
+func _on_animated_sprite_2d_animation_looped():
+	if sprite_2d.animation == "collected":
+		queue_free()
