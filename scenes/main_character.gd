c72b9a69cd3765e5394e93caf2aa571aa1565e89
Leonardo Santos <leobrandonli21@gmail.com>
2023-12-20 19:23:22 -0300

    feat: add fall animation

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -5,16 +5,15 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d = $Sprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
 	move_anim_handler()
-	# Add the gravity.
+	jump_anim_handler()
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -41,3 +40,7 @@ func move_anim_handler():
 		sprite_2d.animation = "running"
 	else:
 		sprite_2d.animation = "default"
+
+func jump_anim_handler():
+	if velocity.y != 0:
+		sprite_2d.animation = "falling" if velocity.y > 0 else "jumping"
